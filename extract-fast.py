1252af7a23607ef490ffc3298f4cad6411a9612a
mahbodez <mahbodissaiy@gmail.com>
2025-05-04 12:05:54 +0330

    modularized the extraction script (load config) and prettified the terminal interface

--- extract-fast.py
+++ extract-fast.py
@@ -11,9 +11,13 @@ import time
 from functools import partial
 import os
 import json
+import yaml
 from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
 from rich.prompt import Prompt, Confirm
 import threading
+import questionary as Q
 
 console = Console()
 colorama.init(autoreset=True)
@@ -98,79 +102,27 @@ def google_get_response(
     return response
 
 
-template_birads = """
-You are a clinical natural language processing (NLP) assistant specialized in radiology report understanding.
-
-Extract the following fields from the mammogram report:
-
-- "left_birads": BI-RADS score for the left breast (0–6 or -1), ignore characters like 'a' or 'b' or 'c'.
-- "right_birads": BI-RADS score for the right breast (0–6 or -1), ignore characters like 'a' or 'b' or 'c'.
-
-Return a JSON with these keys. Only extract what is explicitly stated. If uncertain or not mentioned, use -1. Do not infer.
-
-Report:
-
-<report>
-{report}
-</report>
-
-Make sure to return a JSON response. Do not include any other text or explanation.
-
-"""
-
-template_composition = """
-You are a clinical natural language processing (NLP) assistant specialized in radiology report understanding.
-
-Extract the following fields from the mammogram report:
-
-- "composition": The breast composition category ("A", "B", "C", "D").
-
-Return a valid JSON with this key. 
-Only extract what is explicitly stated. 
-If uncertain or not mentioned, use "Z" as value. 
-Do not infer.
-
-Report:
-
-<report>
-{report}
-</report>
-
-Make sure to return a valid and parseable JSON response. Do not include any other text or explanation.
-"""
-
-template = template_composition
-keys = ["composition"]
-
-models = {
-    "openai-mini": "gpt-4.1-mini",
-    "openai": "gpt-4.1",
-    "google": "models/gemini-2.0-flash",
-}
-
-
 def extract_info(
     ref_df: pd.DataFrame,
     report_col: str,
     number_to_extract: int,
+    template: str,
+    keys: list[str],
     provider: str,
+    model: str,
 ):
     get_response: callable = None
     thread_name = threading.current_thread().name
-    if "openai" in provider:
+    if "openai" in provider.lower():
         client = OpenAI()
-        get_response = partial(
-            openai_get_response, client=client, model=models[provider]
-        )
-    elif "google" in provider:
+        get_response = partial(openai_get_response, client=client, model=model)
+    elif "google" in provider.lower():
         client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
-        get_response = partial(
-            google_get_response, client=client, model=models[provider]
-        )
+        get_response = partial(google_get_response, client=client, model=model)
     else:
         raise ValueError(f"Unsupported provider: {provider}")
     tqdm.write(
-        f"{Fore.BLUE}[{thread_name}]{Style.RESET_ALL} Using {provider} model: {models[provider]}"
+        f"{Fore.BLUE}[{thread_name}]{Style.RESET_ALL} Using {provider} model: {model}"
     )
 
     df = ref_df.copy(deep=True)
@@ -244,6 +196,9 @@ def multi_threaded_fill(
     total_number_to_extract: int,
     number_of_threads: int,
     provider: str,
+    model: str,
+    template: str,
+    keys: list[str],
 ):
     batch_size = total_number_to_extract // number_of_threads
 
@@ -256,12 +211,17 @@ def multi_threaded_fill(
     results = [None] * number_of_threads
     threads = []
 
-    def thread_worker(i, sub_df, report_col, batch_size, provider):
+    def thread_worker(
+        i, sub_df, report_col, batch_size, provider, model, template, keys
+    ):
         df, n = extract_info(
             ref_df=sub_df,
             report_col=report_col,
             number_to_extract=batch_size,
             provider=provider,
+            model=model,
+            template=template,
+            keys=keys,
         )
         if n == 0:
             tqdm.write(f"{Fore.YELLOW}[thread{i}] No more reports to extract")
@@ -270,7 +230,7 @@ def multi_threaded_fill(
     for i, sub_df in enumerate(sub_dfs):
         t = threading.Thread(
             target=thread_worker,
-            args=(i, sub_df, report_col, batch_size, provider),
+            args=(i, sub_df, report_col, batch_size, provider, model, template, keys),
             name=f"thread{i}",
         )
         threads.append(t)
@@ -296,35 +256,88 @@ def multi_threaded_fill(
         tqdm.write(f"{Fore.YELLOW}No results to combine.")
 
 
+def find_csv_files(root: str = "./data"):
+    # find all csv files in the root directory
+    csv_files = [
+        os.path.join(root, f)
+        for f in os.listdir(root)
+        if os.path.isfile(os.path.join(root, f)) and f.lower().endswith(".csv")
+    ]
+    return csv_files
+
+
 def main():
-    ref_csv = Prompt.ask(
-        "Enter the path to the source CSV file with the reports",
-        default="",
-        console=console,
+    console.print(
+        Panel.fit(
+            Text("\U0001f4c4 CSV File Selection", style="bold cyan"),
+            subtitle="Use arrow keys",
+            style="green",
+        )
     )
-    dst_csv = Prompt.ask(
-        "Enter the path to the destination CSV file",
+    ref_csv = Q.select(
+        "Select the reference CSV file",
+        choices=find_csv_files(),
+        use_indicator=True,
+        qmark="❓",
+    ).ask()
+    dst_csv = Q.select(
+        "Select the destination CSV file",
+        choices=find_csv_files() + ["same"],
         default="same",
-        console=console,
-    )
+        use_indicator=True,
+        qmark="❓",
+    ).ask()
     if dst_csv == "same":
         dst_csv = ref_csv
     if not os.path.exists(ref_csv):
         raise FileNotFoundError(f"File not found: {ref_csv}")
     if not os.path.exists(dst_csv):
         raise FileNotFoundError(f"File not found: {dst_csv}")
-    if not Confirm(
-        prompt="Do you want to overwrite the source file?",
+    ref_df = pd.read_csv(ref_csv)
+    console.print(
+        Panel.fit(
+            Text("\u2699\ufe0f YAML Configuration File Selection", style="bold cyan"),
+            subtitle="Fill out",
+            style="green",
+        )
+    )
+    cfg_file = Prompt.ask(
+        "Enter the path to the YAML configuration file",
+        default="extract-cfg.yaml",
         console=console,
-    ):
-        console.clear()
-        main()
-        return
+    )
+    if not os.path.exists(cfg_file) or not cfg_file.lower().endswith(".yaml"):
+        raise FileNotFoundError(f"File not found: {cfg_file}")
+    with open(cfg_file, "r") as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-    report_col = Prompt.ask(
-        "Enter the name of the report column",
+    if "templates" not in cfg or "models" not in cfg:
+        raise ValueError("Invalid configuration file. Missing 'templates' or 'models'.")
+    templates = cfg["templates"]
+    models = cfg["models"]
+
+    console.print(
+        Panel.fit(
+            Text("\U0001f4dc Report Column Selection", style="bold cyan"),
+            subtitle="Use arrow keys",
+            style="green",
+        )
+    )
+
+    report_col = Q.select(
+        "Select the report column",
+        choices=ref_df.columns.tolist(),
         default="report",
-        console=console,
+        use_indicator=True,
+        qmark="❓",
+    ).ask()
+
+    console.print(
+        Panel.fit(
+            Text("\U0001f9f6 Extraction Parameters", style="bold cyan"),
+            subtitle="Fill out",
+            style="green",
+        )
     )
     number_of_threads = Prompt.ask(
         "Enter the number of threads to use for extraction",
@@ -346,22 +359,57 @@ def main():
             f"Total number of reports to extract ({total_number_to_extract}) must be divisible by the number of threads ({number_of_threads})"
         )
 
-    provider = Prompt.ask(
-        "Enter the provider to use for extraction (openai-mini, openai, google)",
-        default=list(models.keys())[0],
+    console.print(
+        Panel.fit(
+            Text("\U0001f916 Template and Model Selection", style="bold cyan"),
+            subtitle="Use arrow keys",
+            style="green",
+        )
+    )
+
+    template = Q.select(
+        "Select the template to use for extraction",
+        choices=list(templates.keys()),
+        default=list(templates.keys())[0],
+        use_indicator=True,
+        qmark="❓",
+    ).ask()
+    if template not in templates:
+        raise ValueError(f"Unsupported template: {template}")
+    template = templates[template]
+    template_prompt = template["prompt"]
+    template_keys = template["keys"]
+
+    model = Q.select(
+        "Select the model to use for extraction",
         choices=list(models.keys()),
+        default=list(models.keys())[0],
+        use_indicator=True,
+        qmark="❓",
+    ).ask()
+    if model not in models:
+        raise ValueError(f"Unsupported model: {model}")
+    provider = models[model]["provider"]
+
+    confirm = Prompt.ask(
+        "Do you want to proceed with the extraction? (yes/no)",
+        default="yes",
         console=console,
     )
-    if provider not in models:
-        raise ValueError(f"Unsupported provider: {provider}")
+    if confirm.lower() != "yes":
+        console.print("Extraction process aborted.")
+        return
 
     multi_threaded_fill(
-        ref_df=pd.read_csv(ref_csv),
+        ref_df=ref_df,
         output_file=dst_csv,
         report_col=report_col,
         total_number_to_extract=total_number_to_extract,
         number_of_threads=number_of_threads,
         provider=provider,
+        model=model,
+        template=template_prompt,
+        keys=template_keys,
     )
 
     tqdm.write(f"{Fore.GREEN}Extraction process completed.")
